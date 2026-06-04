13793d416acc17fe1d5031709298abf0c51c701c
Han-Jeong-Won <jungwon-h@hanmail.net>
2026-06-04 14:46:11 +0900

    login,join

--- src/main/java/com/kh/suje/vo/SellerVO.java
+++ src/main/java/com/kh/suje/vo/SellerVO.java
@@ -23,6 +23,7 @@ public class SellerVO {
     private String openingDate;        // opening_date : DATE (사업자 개업일자)
     private String businessAddress;    // business_address : VARCHAR(255) (사업자 주소)
 
+    
     // private int user_id;
     // private Integer seller_id;
     // private String company_name,business_number,status,created_at;
